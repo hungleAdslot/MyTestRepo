dc2d2aea9d6f40c36f8b54f60654f3652fef65a7
Hung.Le <Hung.Hoa@adslot.com>
2019-05-07 10:19:28 +0700

    addcode

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,30 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            // update change asdaas 
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            // Create an array of all supported standard date and time format specifiers.
+            string[] formats = {"D"};
+            // Create an array of four cultures.                                 
+            CultureInfo[] cultures = {CultureInfo.CreateSpecificCulture("de-DE"),
+                                CultureInfo.CreateSpecificCulture("en-US"),
+                                CultureInfo.CreateSpecificCulture("es-ES"),
+                                CultureInfo.CreateSpecificCulture("zh-CN"),
+                                CultureInfo.CreateSpecificCulture("fr-FR")};
+            // Define date to be displayed.
+            DateTime dateToDisplay = DateTime.Now;
+
+            // Iterate each standard format specifier.
+            foreach (string formatSpecifier in formats)
+            {
+                foreach (CultureInfo culture in cultures)
+                    Console.WriteLine("{0} Format Specifier {1, 10} Culture {2, 40} {3}",
+                                      formatSpecifier, culture.Name,
+                                      dateToDisplay.ToString(formatSpecifier, culture),
+                                      culture.DateTimeFormat.GetDayName(dateToDisplay.DayOfWeek));
+                
+                Console.WriteLine();
+            }
+            Console.ReadLine();
         }
     }
 }
